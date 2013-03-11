d2487db65567c23fb358a81daf313c026bef5af0
Uso <matthias.gatto@epitech.eu>
2013-03-11 23:05:08 +0100

    handle file name in output

--- tool/downscale_480p.sh
+++ tool/downscale_480p.sh
@@ -7,7 +7,7 @@ OLD_DIR_NAME=`date | tr ' :' '_'`_old
 GET_HEIGHT="ffprobe -v 0 -show_streams \"\$i\" | grep height | awk -F'=' '{ print \$2 }'"
 GET_WIDTH="ffprobe -v 0 -show_streams \"\$i\" | grep width | awk -F'=' '{ print \$2 }'"
 
-CONVERT="ffmpeg -i \"$OLD_DIR_NAME/\$i\" -f avi -vcodec libxvid -b:v 3500k -s \$NEW_WIDTH:480 -acodec libmp3lame -ab 128000 -ar 44100 output.avi"
+CONVERT="ffmpeg -i \"$OLD_DIR_NAME/\$i\" -f avi -vcodec libxvid -b:v 3500k -s \$NEW_WIDTH:480 -acodec libmp3lame -ab 128000 -ar 44100 \"\$BASE\""
 
 
 if [ "$#" -lt 1 ]; then
@@ -26,7 +26,8 @@ for i in *.avi *.mp4 *.mkv *.ogg *.flv; do
     HEIGHT=`eval $GET_HEIGHT`
     WIDTH=`eval $GET_WIDTH`
     NEW_WIDTH=`expr $WIDTH \* 480 / $HEIGHT / 8 \* 8`
-
+    EXT=`echo $i | awk  -F"." '{print $NF}'`
+    BASE=`basename "$i" .$EXT`.avi
 
     if [ "$HEIGHT" -gt 500 ]; then
 	mv -v "$i" $OLD_DIR_NAME;
