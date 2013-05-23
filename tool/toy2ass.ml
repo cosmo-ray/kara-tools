51b54236083fee2f6e39cc57763e76c79d7382e7
Uso <matthias.gatto@epitech.eu>
2013-05-23 02:37:43 +0200

    add converter

--- tool/toy2ass.ml
+++ tool/toy2ass.ml
@@ -31,3 +31,48 @@ let phrase2string lfrm llyr fps =
 ;;
 
 
+let file2list f = let r = ref [] in let a = open_in f in let _ = try while true do r:=(input_line a)::(!r) done with _->close_in a in List.rev (!r);;
+
+let frm1line s = let i = String.index s ' ' in int_of_string(String.sub s 0 i),int_of_string(String.sub s (i+1) (String.length s - i - 1));;
+
+let lyr1line s = let aux1 s = let i = String.index s '&' in String.sub s 0 i,String.sub s (i+1) (String.length s - i - 1) in
+		 let rec aux2 current res = try let a,b=aux1 current in aux2 b (a::res) with _->current::res in
+		 List.tl (List.rev (aux2 s []))
+;;
+let decoupephrases l l2 = 
+  let ll = List.map List.length l2 in
+  let rec aux l n ln r lr = if n=0 then if ln=[] then List.rev(List.rev(r)::lr) else (aux l (List.hd ln) (List.tl ln) [] (List.rev(r)::lr)) else aux (List.tl l) (n-1) ln (List.hd l::r) lr in
+  aux l (List.hd ll) (List.tl ll) [] []
+;;
+
+let toy2ass lyrFile frmFile fps = 
+  let lyrics = List.filter (fun x->x<>[]) (List.map lyr1line (file2list lyrFile)) in
+  let frames = decoupephrases (List.map frm1line (file2list frmFile)) (lyrics) in
+  let mix = List.combine lyrics frames in
+  List.map (fun (a,b)->phrase2string b a fps) mix
+;;
+
+let assHeader =
+["[Script Info]";
+"Title: Default Aegisub file";
+"ScriptType: v4.00+";
+"WrapStyle: 0";
+"ScaledBorderAndShadow: yes";
+"Collisions: Normal";
+"Scroll Position: 1";
+"Active Line: 7";
+"Video Zoom Percent: 1";
+"Last Style Storage: Default";
+"Audio URI: Sukeban Deka II ed 3.avi";
+"YCbCr Matrix: None";
+"";
+"[V4+ Styles]";
+"Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding";
+"Style: Default,Arial,20,&H00FFFFFF,&H000000FF,&H00000000,&H00000000,0,0,0,0,100,100,0,0,1,2,2,8,10,10,10,1";
+"";
+"[Events]";
+"Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text"];;
+
+let writefile f l = let a = open_out f in List.iter (fun x->output_string a (x^"\n")) l;close_out a;;
+
+let writeAssToFile assStrings file =   writefile file (assHeader@assStrings);;
